4545b188133eca9fa2284ed7916ee8833d71f165
Jorge Gomes <jorge.gomes@vost.pt>
2021-04-19 17:42:33 +0100

    Create covid19_story.py
    Python script that allows to visualize COVID-19 data in Portugal. This graph shows the cases per 100k inhabitants and the R(t) for the county's ARS (health region)

---
+++ covid19_story.py
@@ -0,0 +1,39 @@
+# Import all libraries 
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
+
+# Read CSV file   
+df = pd.read_csv('COVID19PT_STORY.csv')
+
+# CSV file has the following fields 
+# DATA - Date 
+# CONCELHO - County
+# ARS - Health Region 
+# Valor - Cases per 100,000k inhabitants during a 14 day period 
+# RT - The R(t) value for ARS at the 'DATA'
+
+# Make sure that 'DATA' is in the right format 
+
+df['DATA'] = pd.to_datetime(df['DATA'],format='%Y-%m-%d')
+print(df.info())
+
+# Make Graph
+# Notes: Due to limitations on plotly we turn the field 'DATA' into a string so that the animation plays nicely 
+
+fig = px.scatter(df, 
+	x="RT", 
+	y="VALOR",
+	animation_frame=df.DATA.astype(str),
+	animation_group="CONCELHO",
+    size="VALOR", 
+    color="CONCELHO", 
+    hover_name="CONCELHO",
+    facet_col = "ARS",
+    log_x=False, 
+    size_max=45, 
+    range_x=[0,2],
+    range_y=[0,2400])
+
+#Plot Graph 
+fig.show()
